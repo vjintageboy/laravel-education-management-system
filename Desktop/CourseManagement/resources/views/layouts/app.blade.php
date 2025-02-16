683ad90cb93dc27d5ba7ce2a64f7d1e94ca89e7d
Pham Hoang Anh <117793920+vjintageboy@users.noreply.github.com>
2025-02-16 23:22:48 +0700

    Add profile edit view with update, password, and delete forms

--- Desktop/CourseManagement/resources/views/layouts/app.blade.php
+++ Desktop/CourseManagement/resources/views/layouts/app.blade.php
@@ -21,7 +21,31 @@
     <div class="content-wrapper">
         <section class="content">
             <div class="container-fluid">
-                @yield('content')
+                @if(request()->routeIs('profile.edit'))
+                    <div class="py-12">
+                        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-profile-information-form', ['user' => auth()->user()])
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.update-password-form', ['user' => auth()->user()])
+                                </div>
+                            </div>
+
+                            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                                <div class="max-w-xl">
+                                    @include('profile.partials.delete-user-form', ['user' => auth()->user()])
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    @yield('content')
+                @endif
             </div>
         </section>
     </div>
