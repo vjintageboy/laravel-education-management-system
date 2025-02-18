48e050af2a17b2a3445a28374b157133ccff14a9
Pham Hoang Anh <117793920+vjintageboy@users.noreply.github.com>
2025-02-18 09:24:29 +0700

    fix errro no css when deloyment to ngrok

--- Desktop/CourseManagement/vite.config.js
+++ Desktop/CourseManagement/vite.config.js
@@ -2,10 +2,29 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/build/' : 'https://2422-27-72-104-107.ngrok-free.app/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].[hash].js`,
+                chunkFileNames: `[name].[hash].js`,
+                assetFileNames: `[name].[hash].[ext]`,
+            },
+        },
+    },
+    server: {
+        hmr: {
+            host: '2422-27-72-104-107.ngrok-free.app',
+            protocol: 'wss'
+        }
+    }
 });
